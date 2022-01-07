2d4c82dc3321f1ffe2a508f8c47da537885ce697
Yechan  Jeon <65900882+codernineteen@users.noreply.github.com>
2022-01-07 23:02:27 +0900

    working with video customization

--- client/src/components/Browse/Browse.js
+++ client/src/components/Browse/Browse.js
@@ -1,8 +1,12 @@
-import { useEffect } from "react";
+import { Fragment, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { checkUserAuthenticated } from "../../api";
+import DontLookUp from "../../assets/video/dont.mp4";
+
+import classes from "./Browse.module.css";
 
 const Browse = () => {
+  const videoRef = useRef();
   const navigate = useNavigate();
   useEffect(() => {
     const getResult = async () => {
@@ -13,7 +17,20 @@ const Browse = () => {
     };
     getResult();
   }, [navigate]);
-  return <button>Send request</button>;
+
+  const playVideo = () => {
+    videoRef.play();
+  };
+  return (
+    <Fragment>
+      <video loop muted={false} ref={videoRef}>
+        <source src={DontLookUp} type="video/mp4" />
+      </video>
+      <button className={classes.play} onClick={playVideo}>
+        play
+      </button>
+    </Fragment>
+  );
 };
 
 export default Browse;
